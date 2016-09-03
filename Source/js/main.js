ecdaea60215629f0b82c2614e570c7691cf685fe
Dương Diệu Pháp <d2phap@gmail.com>
2016-09-03 13:22:53 +0800

    game beta

--- Source/js/main.js
+++ Source/js/main.js
@@ -4,33 +4,91 @@
 */
 "use strict";
 
-var _card_1 = -1;
-var _card_2 = -1;
-var _card_3 = -1;
+var _nums = [-1, -1, -1];
+var _timer;
+var _punishments = [
+    "5 sweet kisses",
+    "Bite ears",
+    "Squeeze butt",
+    "Lick nipples",
+    "Tickle armpits"
+];
 
 //initiate app
-$(document).ready(function() {
+$(document).ready(function () {
 
-	//generate randomly number from 0-9
+    //initialize game
+    initGame();
 
+    //onClick event for cards
+    $(".flip-container").on("click", function () {
+        if (_timer) {
+            clearTimeout(_timer);
+        }
 
-	//onClick event for cards
-	$(".flip-container").on("click", function() {
-		var id = $(this).data("id");
+        var id = $(this).data("id");
+        var value = $(this).find(".flipper .back").data("value");
 
-		//Game end
-		if (_card_1 != -1 && _card_2 != -1 && _card_3 != -1) {
-			$("body").addClass("game-end");
-		}
-		//Game play
-		else {
-			$(this).addClass("hover");
+        //get value
+        _nums[id] = value;
 
-			
-		}
-	});
+        // flip and lock it
+        $(this).addClass("hover");
 
+        console.log(_nums);
 
+        // check game end
+        _timer = setTimeout(isGameEnd, 2000);
+    });
 
+    $("#btnPunishment").on("click", function () {
+        var punishment_id = createRandomNumbers(0, _punishments.length - 1);
+        var sum = $("#card-1 .flipper .back").data("value");
+
+        $("#card-1 .flipper .back").text(_punishments[punishment_id]);
+        $("#card-1 .flipper .back").attr("data-value", "for " + sum + "s");
+        $("body").addClass("punishment");
+
+        $("#btnPunishment").addClass("hidden");
+        $("#btnReplay").removeClass("hidden");
+    });
+
+    $("#btnReplay").on("click", function () {
+        initGame();
+    });
 
 });
+
+function initGame() {
+    //reset values
+    _nums = [-1, -1, -1];
+    $("#btnPunishment").addClass("hidden");
+    $("#btnReplay").addClass("hidden");
+    $("body").removeClass("game-end punishment");
+    $(".flip-container").removeClass("hover");
+    $("#card-1 .flipper .back").text("");
+
+    for (var i = 0; i <= 2; i++) {
+        var value = createRandomNumbers(0, 9);
+
+        $("#card-" + i).find(".flipper .back").attr("data-value", value);
+    }
+}
+
+function createRandomNumbers(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isGameEnd() {
+    //Game end
+    if (_nums.lastIndexOf(-1) == -1) {
+
+        var sum = _nums.reduce(function (previousValue, currentValue, currentIndex, array) {
+            return previousValue + currentValue;
+        });
+        $("#card-1 .flipper .back").attr("data-value", sum);
+
+        $("body").addClass("game-end");
+        $("#btnPunishment").removeClass("hidden");
+    }
+}
